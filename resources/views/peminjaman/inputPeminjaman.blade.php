bcd824fcab5d66c41cc5759a078ec7e5560d7bd8
afifridho <afifridhokamalputra@gmail.com>
2017-07-06 14:36:31 +0700

    calendar input peminjaman fixed

--- resources/views/peminjaman/inputPeminjaman.blade.php
+++ resources/views/peminjaman/inputPeminjaman.blade.php
@@ -98,65 +98,16 @@
 <!-- bootstrap datepicker -->
 <script src="../../plugins/datepicker/bootstrap-datepicker.js"></script>
 <script type="text/javascript">
-function getdate() {
-  var tt = document.getElementById('datepicker').value;
-
-  var date = new Date(tt);
-  var newdate = new Date(date);
-  var deadline = "<?php echo Session::get('totaltikpro');?>"
-  newdate.setDate(newdate.getDate() + parseInt(deadline));
-
-  var dd = newdate.getDate();
-  var mm = newdate.getMonth() + 1;
-  var y = newdate.getFullYear();
-  console.log(dd)
-  console.log(mm)
-  console.log(y)
-
-  var FormattedDate = y + '-' + mm + '-' + dd;
-    document.getElementById('datedead').value = FormattedDate;
-}
-
-
-  var tt = document.getElementById('datepicker1').value;
-
-  var date = new Date(tt);
-  var newdate = new Date(date);
-  var deadline = "<?php echo Session::get('totaltikpro');?>"
-  newdate.setDate(newdate.getDate() + parseInt(deadline));
-
-  var dd = newdate.getDate();
-  var mm = newdate.getMonth() + 1;
-  var y = newdate.getFullYear();
-  console.log(dd)
-  console.log(mm)
-  console.log(y)
-
-  var FormattedDate = y + '-' + mm + '-' + dd;
-    document.getElementById('datedead').value = FormattedDate;
-}
 
 $(function () {
-      $('#daterange-btn').daterangepicker(
-          {
-            ranges: {
-              'Today': [moment(), moment()],
-              'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-              'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-              'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-              'This Month': [moment().startOf('month'), moment().endOf('month')],
-              'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-            },
-            startDate: moment().subtract(29, 'days'),
-            endDate: moment()
-          },
-          function (start, end) {
-            $('#daterange-btn span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-          }
-      );
-
-      //Date picker
+      
       $('#datepicker').datepicker({
+        format: 'yyyy-mm-dd',
+        changeMonth: true,
+        changeYear: true,
+        autoclose: true
+      });
+      $('#datepicker1').datepicker({
         format: 'yyyy-mm-dd',
         changeMonth: true,
         changeYear: true,
