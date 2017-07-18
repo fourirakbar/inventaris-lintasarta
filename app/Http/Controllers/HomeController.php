d2bdc9c77bc242ec15f0efe10634b24ad43b8677
afifridho <afifridhokamalputra@gmail.com>
2017-07-18 17:21:58 +0700

    home monitoring add permintaan

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -46,17 +46,62 @@ class HomeController extends Controller
     public function showminta()
     {	
     	$jebret = Permintaan::query()->join('TIKPRO','TIKPRO.ID_TIKPRO','=','PERMINTAAN.TIKPRO_ID')->get(); //ambil data dari table PERMINTAAN dan table TIKPRO dengan ketentuan yang sudah diberikan
-        $jebret2 = DB::table('TIKPRO')->get(); //ambil semua data dari tabel TIKPRO
+        $jebret3 = Permintaan::query()->join('TIKPRO','TIKPRO.ID_TIKPRO','=','PERMINTAAN.TIKPRO_ID')->select('PERMINTAAN.ID_PERMINTAAN')->get();
+        $jebret2 = array();
+        foreach ($jebret3 as $key) {
+            $jebret2a = DB::table('HISTORY_TIKPRO')->join('PERMINTAAN', 'PERMINTAAN.ID_PERMINTAAN','=','HISTORY_TIKPRO.PERMINTAAN_ID')->where('HISTORY_TIKPRO.PERMINTAAN_ID',$key->ID_PERMINTAAN)->get(); //ambil semua data dari tabel TIKPRO
+            array_push($jebret2, $jebret2a);
+        }
         $deadline = array();
-	    for ($i = 1 ; $i <= count($jebret2) ; $i++){
-	      if (empty($deadline)) {
-	        array_push($deadline, $jebret2[$i-1]->DEADLINE);
-       	  }
-	      else{
-	          array_push($deadline, $jebret2[$i-1]->DEADLINE + $deadline[$i-2]); 
-	      }
-	    }
-        return response()->json(['success' => true, 'minta' => "coming soon"]);
+		$deadline2 = array();
+		$deadline3 = array();
+		$deadline4 = array();
+		foreach ($jebret2 as $key) {
+		foreach ($key as $value) {
+		  array_push($deadline, ["idpermintaan" => $value->ID_PERMINTAAN, "deadline" => $value->DEADLINE, "idtikpro" => $value->TIKPRO_ID]);
+		}
+		array_push($deadline2, $deadline);
+		$deadline = array();
+
+		}
+		for ($i=1; $i <= count($deadline2) ; $i++) {
+		for ($j=1; $j <= count($deadline2[$i-1]) ; $j++) { 
+		  
+		  if (empty($deadline3)) {
+		    array_push($deadline3, ["idpermintaan" => $deadline2[$i-1][$j-1]["idpermintaan"], "deadline" => $deadline2[$i-1][$j-1]["deadline"], "idtikpro" => $deadline2[$i-1][$j-1]["idtikpro"]]);
+		  }
+		  else{
+		    array_push($deadline3, ["idpermintaan" => $deadline2[$i-1][$j-1]["idpermintaan"], "deadline" => $deadline2[$i-1][$j-1]["deadline"]+ $deadline3[$j-2]["deadline"], "idtikpro" => $deadline2[$i-1][$j-1]["idtikpro"]]); 
+		  }
+		}
+		array_push($deadline4, $deadline3);
+		$deadline3 = array();
+		}
+
+		$counter = 0;
+		foreach ($jebret as $key) {
+			if ($key->STATUS == "in progress") {
+			$date1=date_create();
+			$date2=date_create($key->TGL_PERMINTAAN);
+			foreach ($deadline4 as $jumlaharray) {
+				for ($i=0; $i < count($jumlaharray) ; $i++) { 
+					if ($key->TIKPRO_ID == $i && $key->ID_PERMINTAAN == $jumlaharray[$i]["idpermintaan"]) {
+					   $deaddead = $jumlaharray[$i]["deadline"];
+			  			// echo "true";
+				  	}
+				}
+			}
+			// echo '<td style="background-color: green; color: white; text-align: center; vertical-align: middle;" >',$deaddead,'</td>';
+			$new = date_add($date2,date_interval_create_from_date_string((string)((int)$deaddead-3)." days"));
+			$diff=date_diff($date1,$new);
+			$print = $diff->format('%R%a Hari');
+			if ($print <=0) {
+				$counter++;
+			}
+			}
+		}
+
+        return response()->json(['success' => true, 'minta' => $counter]);
     }
 
     public function showpinjam()
