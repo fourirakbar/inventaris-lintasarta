1376e12f1a92b2bbcc516fcc90abb71608d3b650
Unknown <afifridhokamalputra@gmail.com>
2017-08-03 04:01:13 +0700

    add comment

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,70 +17,63 @@ use Session;
 
 class HomeController extends Controller
 {
+    //fungsi untuk menampilkan halaman home
     public function index()
     {
     	return view('home');
         // echo $counter;
     }
 
-    // public function showbatal()
-    // {
-    //     $createnow = date_create();
-    //     $datenow = date_format($createnow, 'Y-m-d');
-    //     $pembatalandate = DB::table('PEMBATALAN')->select('updated_at')->get();
-    //     $arraydatebatal=array();
-    //     foreach ($pembatalandate as $batal) {
-    //     	array_push($arraydatebatal, date_format(date_create($batal->updated_at), 'Y-m-d'));
-    //     }
-    //     $counter = 0;
-    //     foreach ($arraydatebatal as $arraybatal) {
-    //     	$diff=date_diff(date_create($datenow),date_create($arraybatal));
-    //     	$beda = $diff->format("%a");
-    //     	if ($beda == 0) {
-    //     		$counter++;
-    //     	}
-    //     }
-    //     return response()->json(['success' => true, 'batal' => $counter]);
-    // }
-
+    //fungsi untuk menghitung permintaan pembatalan yang belum diproses
     public function showbatal()
     {
+        //list semua data permintaan yang dibatalkan dan belum di proses
         $pembatalan = DB::table('PEMBATALAN')->select('*')->where('STATUS_PEMBATALAN', '=', 'in progress')->get();
+        //menghitung banyak data pada variabel $pembatalan
         $counter = count($pembatalan);
+        //mengirim response dalam bentuk json ke javascript di view/halaman 'home'
         return response()->json(['success' => true, 'batal' => $counter]);
     }
 
     public function showminta()
-    {	
+    {	//query - pilih data pada tabel permintaan yang di join dengan tabel tikpro dengan ketentuan id tikpro pada tabel permintaan sama dengan id tikpro pada tabel history tikpro dan id permintaan pada tabel permintaan sama dengan id permintaan pada tabel history tikpro (untuk listing seluruh data permintaan)
     	$jebret = DB::select("select * from PERMINTAAN inner join HISTORY_TIKPRO where PERMINTAAN.TIKPRO_ID = HISTORY_TIKPRO.TIKPRO_ID and PERMINTAAN.ID_PERMINTAAN = HISTORY_TIKPRO.PERMINTAAN_ID order by PERMINTAAN.ID_PERMINTAAN DESC");
 
+        //query - pilih data pada tabel permintaan yang di join dengan tabel tikpro dengan ketentuan id tikpro pada tabel permintaan sama dengan id tikpro pada tabel history tikpro dan id permintaan pada tabel permintaan sama dengan id permintaan pada tabel history tikpro (untuk listing seluruh data permintaan) 
         $jebret3 = DB::select("select ID_PERMINTAAN from PERMINTAAN inner join HISTORY_TIKPRO where PERMINTAAN.TIKPRO_ID = HISTORY_TIKPRO.TIKPRO_ID and PERMINTAAN.ID_PERMINTAAN = HISTORY_TIKPRO.PERMINTAAN_ID");
 
+        //query - pilih data pada tabel permintaan yang di join dengan tabel tikpro dengan ketentuan id permintaan pada tabel permintaan sama dengan id permintaan pada tabel history tikpro (untuk mengambil data pada kolom tanggal selesai)
         $tglselesai = DB::select("select * from HISTORY_TIKPRO join PERMINTAAN on PERMINTAAN.ID_PERMINTAAN = HISTORY_TIKPRO.PERMINTAAN_ID");
-        // dd($tglselesai);
-        // dd($jebret);
-        // dd($jebret);
-        // dd($jebret); //ambil data dari table PERMINTAAN dan table TIKPRO dengan ketentuan yang sudah diberikan
-        // $jebret3 = Permintaan::query()->join('TIKPRO','TIKPRO.ID_TIKPRO','=','PERMINTAAN.TIKPRO_ID')->select('PERMINTAAN.ID_PERMINTAAN')->get();
-        // dd($jebret3);
+        
+        //inisiasi array
         $jebret2 = array();
+
+        //looping seluruh data pada variabel $jebret3 sebagai variabel $key
         foreach ($jebret3 as $key) {
-            $jebret2a = DB::table('HISTORY_TIKPRO')->join('PERMINTAAN', 'PERMINTAAN.ID_PERMINTAAN','=','HISTORY_TIKPRO.PERMINTAAN_ID')->where('HISTORY_TIKPRO.PERMINTAAN_ID',$key->ID_PERMINTAAN)->get(); //ambil semua data dari tabel TIKPRO
+            $jebret2a = DB::table('HISTORY_TIKPRO')->join('PERMINTAAN', 'PERMINTAAN.ID_PERMINTAAN','=','HISTORY_TIKPRO.PERMINTAAN_ID')->where('HISTORY_TIKPRO.PERMINTAAN_ID',$key->ID_PERMINTAAN)->get();
+            //memasukkan seluruh data pada variabel $jebret2a kedalam array $jebret2
             array_push($jebret2, $jebret2a);
         }
 
+        //inisiasi array
         $deadline = array();
-          $deadline2 = array();
-          $deadline3 = array();
-          $deadline4 = array();
+        $deadline2 = array();
+        $deadline3 = array();
+        $deadline4 = array();
+        //looping seluruh data array pada variabel $jebret2
           foreach ($jebret2 as $key) {
+            //looping seluruh data array pada setiap array yang didifinisikan dalam variabel $key
             foreach ($key as $value) {
+            //memasukkan seluruh ID_PERMINTAAN, deadline, id tikpro pada setiap data permintaan ke variabel $deadline
               array_push($deadline, ["idpermintaan" => $value->ID_PERMINTAAN, "deadline" => $value->DEADLINE, "idtikpro" => $value->TIKPRO_ID]);
             }
+            //memasukkan variabel $deadline setiap loop kedalam variabel $deadline2
             array_push($deadline2, $deadline);
+            //mengosongkan variabel $deadline agar mereset setiap looping
             $deadline = array();
 
           }
+          //masukkan seluruh idpermintaan, deadline, id tikpro ke variabel $deadline4
           for ($i=1; $i <= count($deadline2) ; $i++) {
             for ($j=1; $j <= count($deadline2[$i-1]) ; $j++) {
 
@@ -94,6 +87,7 @@ class HomeController extends Controller
             array_push($deadline4, $deadline3);
             $deadline3 = array();
           }
+          //looping ke seluruh data, mengambil tanggal selesai dari data permintaan
           foreach ($jebret as $key) {
               $arraytglselesai = array();
             foreach ($tglselesai as $index) {
@@ -101,14 +95,13 @@ class HomeController extends Controller
                 array_push($arraytglselesai, $index->TGL_SELESAI);
               }
             }
-            // dd($arraytglselesai);
+            //mencari tanggal selesai pada tikpro yang terakhir dilaksanakan
             foreach ($arraytglselesai as $index) {
               if($index != NULL){
                 $tglselesaiterakhir = $index;
               }
             }
-            // dd($tglselesaiterakhir);
-
+            //menghitung seluruh data yang statusnya 'in progress' dan titik prosesnya melewati batas waktu
              if ($key->STATUS == "in progress") {
                  
                  $counter = 0;
@@ -123,7 +116,7 @@ class HomeController extends Controller
                         $new = date_add($date2,date_interval_create_from_date_string((string)((int)$deaddead)." days"));
                         $diff=date_diff($date1,$new);
                         $print = $diff->format('%R%a Hari');
-                        if ($print < 0) {
+                        if ($print <= 0) {
                              $counter++;
                          } 
                       }
@@ -134,7 +127,7 @@ class HomeController extends Controller
                         $new = date_add($date2,date_interval_create_from_date_string((string)((int)$deaddead)." days"));
                         $diff=date_diff($date1,$new);
                         $print = $diff->format('%R%a Hari');
-                        if ($print < 0) {
+                        if ($print <= 0) {
                              $counter++;
                          }
                       }
@@ -143,10 +136,11 @@ class HomeController extends Controller
                 }
             }
           }
-          
+        //mengirim response dalam bentuk json ke javascript di view/halaman 'home'
         return response()->json(['success' => true, 'minta' => $counter]);
     }
 
+    //fungsi untuk menghitung peminjaman yang lebih dari batas waktu
     public function showpinjam()
     {
         $createnow = date_create();
@@ -170,8 +164,9 @@ class HomeController extends Controller
         return response()->json(['success' => true, 'pinjam' => $counter]);
     }
 
+    //fungsi untuk menghitung barang yang sedang diperbaiki yang melewati batas waktu
     public function showrepair()
-    {
+    {   
         $createnow = date_create();
         $datenow = date_format($createnow, 'Y-m-d');        
         $repairreturn = DB::table('REPAIR')->select('PERKIRAAN_SELESAI')->where('STATUS_REPAIR', '!=', 'Done')->get();
@@ -180,12 +175,10 @@ class HomeController extends Controller
         	$new = date_add(date_create($return->PERKIRAAN_SELESAI),date_interval_create_from_date_string("1 days"));
         	array_push($arraydaterepair, date_create($return->PERKIRAAN_SELESAI));
         }
-        // $arraydebug = array();
         $counter = 0;
         foreach ($arraydaterepair as $arrayreturn) {
         	$diff=date_diff(date_create($datenow),$arrayreturn);        	
         	$beda = $diff->format("%R%a");
-        	// array_push($arraydebug, $beda);
         	if ($beda <= 0) {
         		$counter++;
         	}
@@ -193,6 +186,7 @@ class HomeController extends Controller
         return response()->json(['success' => true, 'repair' => $counter]);
     }
 
+    //fungsi untuk menampilkan data peminjaman yang lewat dari batas waktu
     public function showpinjam2(){
         $peminjaman = DB::select("SELECT * FROM `PEMINJAMAN` WHERE TGL_PENGEMBALIAN < NOW() AND KETERANGAN != 'done' ");
         // dd($peminjaman);
@@ -200,28 +194,25 @@ class HomeController extends Controller
         
     }
 
+    //fungsi untuk menampilkan data perbaikan yang lewat dari batas waktu
     public function showrepair2(){
         $repair = DB::select("SELECT * FROM `REPAIR` WHERE PERKIRAAN_SELESAI < NOW() AND STATUS_REPAIR != 'Done' ");
         // dd($peminjaman);
         return view('repair.showRepair', compact('repair'));
         
     }
+
+    //fungsi untuk menampilkan data permintaan yang lewat dari batas waktu
     public function showminta2() {
         $tglselesai = DB::select("select * from HISTORY_TIKPRO join PERMINTAAN on PERMINTAAN.ID_PERMINTAAN = HISTORY_TIKPRO.PERMINTAAN_ID");
         $jebret = DB::select("select * from PERMINTAAN inner join HISTORY_TIKPRO where PERMINTAAN.TIKPRO_ID = HISTORY_TIKPRO.TIKPRO_ID and PERMINTAAN.ID_PERMINTAAN = HISTORY_TIKPRO.PERMINTAAN_ID and STATUS = 'in progress' and DATE_ADD(PERMINTAAN.TGL_PERMINTAAN, INTERVAL DEADLINE_NEW DAY) < NOW() order by PERMINTAAN.ID_PERMINTAAN DESC");
 
         $jebret3 = DB::select("select ID_PERMINTAAN from PERMINTAAN inner join HISTORY_TIKPRO where PERMINTAAN.TIKPRO_ID = HISTORY_TIKPRO.TIKPRO_ID and PERMINTAAN.ID_PERMINTAAN = HISTORY_TIKPRO.PERMINTAAN_ID");
-        // dd($jebret);
-        // dd($jebret);
-        // dd($jebret); //ambil data dari table PERMINTAAN dan table TIKPRO dengan ketentuan yang sudah diberikan
-        // $jebret3 = Permintaan::query()->join('TIKPRO','TIKPRO.ID_TIKPRO','=','PERMINTAAN.TIKPRO_ID')->select('PERMINTAAN.ID_PERMINTAAN')->get();
-        // dd($jebret3);
         $jebret2 = array();
         foreach ($jebret3 as $key) {
             $jebret2a = DB::table('HISTORY_TIKPRO')->join('PERMINTAAN', 'PERMINTAAN.ID_PERMINTAAN','=','HISTORY_TIKPRO.PERMINTAAN_ID')->where('HISTORY_TIKPRO.PERMINTAAN_ID',$key->ID_PERMINTAAN)->get(); //ambil semua data dari tabel TIKPRO
             array_push($jebret2, $jebret2a);
         }
-        // dd($jebret2);
 
         return view('permintaan.semuaPermintaan', compact('jebret', 'jebret2', 'tglselesai')); //return view ke halaman semuaPermintaan dengan data dari variable $jebret dan $jebret2
     }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,16 +13,24 @@ use Session;
 use App\Permintaan;
 
 class UserController extends Controller
-{
+{   
+    //fungsi untuk menampilkan halaman view usersearch
     public function index(){
     	return view('userinterface.usersearch');
     }
 
+    //fungsi untuk menampilkan data sesuai dengan nomor tiket
     public function show(Request $request){
+        //listing data pada tabel permintaan sesuai dengan nomor tiket yagn diinput
     	$permintaan = DB::table('PERMINTAAN')->select('*')->where('NOMOR_TICKET', '=', $request->NO_TIKET)->get();
-    	$peminjaman = DB::table('PEMINJAMAN')->select('*')->where('NOMOR_TICKET', '=', $request->NO_TIKET)->get();
-    	$repair = DB::table('REPAIR')->select('*')->where('NOMOR_TICKET', '=', $request->NO_TIKET)->get();
-    	if (count($permintaan) != 0) {
+    	//listing data pada tabel peminjman sesuai dengan nomor tiket yagn diinput
+        $peminjaman = DB::table('PEMINJAMAN')->select('*')->where('NOMOR_TICKET', '=', $request->NO_TIKET)->get();
+    	//listing data pada tabel repair sesuai dengan nomor tiket yagn diinput
+        $repair = DB::table('REPAIR')->select('*')->where('NOMOR_TICKET', '=', $request->NO_TIKET)->get();
+    	
+        //cek apakah ada data pada variabel $permintaan
+        if (count($permintaan) != 0) {
+            //show permintaan berdasarkan nomor tiket yang diinput
     		$showdata = Permintaan::query()->join('TIKPRO','TIKPRO.ID_TIKPRO','=','PERMINTAAN.TIKPRO_ID')->where('NOMOR_TICKET', '=', $request->NO_TIKET)->get();
     		$jebret2 = DB::table('HISTORY_TIKPRO')->where('PERMINTAAN_ID', $permintaan[0]->ID_PERMINTAAN)->get(); //ambil semua data dari tabel TIKPRO
             $boi = DB::table('HISTORY_TIKPRO')->select('*')->join('PERMINTAAN','PERMINTAAN.ID_PERMINTAAN', '=', 'HISTORY_TIKPRO.PERMINTAAN_ID')->where('PERMINTAAN.ID_PERMINTAAN', $permintaan[0]->ID_PERMINTAAN)->get(); //ambil data dari table HISTORY_TIKPRO dan table PERMINTAAN dengan ketentuan yang sudah diberikan
@@ -30,26 +38,21 @@ class UserController extends Controller
             $count = DB::table('HISTORY_TIKPRO')->where('PERMINTAAN_ID', $permintaan[0]->ID_PERMINTAAN)->count();
 	        return view('permintaan.usershowpermintaan', compact('showdata', 'jebret2', 'boi', 'count'));
     	}
+        //cek apakah ada data pada variabel $peminjaman$
     	elseif (count($peminjaman) != 0){
+            //show permintaan berdasarkan nomor tiket yang diinput
     		$peminjaman = DB::table('PEMINJAMAN')->select('*')->where('NOMOR_TICKET', '=', $request->NO_TIKET)->get();
-            // dd($peminjaman);
-	        return view('peminjaman.usershowpeminjaman', compact('peminjaman'));
+            return view('peminjaman.usershowpeminjaman', compact('peminjaman'));
     	}
+        //cek apakah ada data pada variabel $repair
     	elseif (count($repair) != 0){
     		$data = $repair[0];
-            // dd($data);
+            //show permintaan berdasarkan nomor tiket yang diinput
             return view('repair.usershowrepair', compact('data')); //return view halaman showRepair
     	}
         else{
+            //return not found jika nomor tiket yang diinput tidak ada dalam database
             return view('userinterface.usersearchnotfound');
         }
-    }
-
-    public function showCancel(Request $request) {
-        dd($request);
-    }
-
-    public function cancel() {
-
     }
 }
