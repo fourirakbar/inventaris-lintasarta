55d29e407c9e213ce326431d81f8c97639c670a4
Unknown <afifridhokamalputra@gmail.com>
2017-08-08 10:06:12 +0700

    ganti nama app

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -2,7 +2,7 @@
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><img src="{{URL::asset('dist/img/logo.jpg')}}"></span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>SI</b>DOMO</span>
+      <span class="logo-lg"><b>SI</b>NTARI</span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
